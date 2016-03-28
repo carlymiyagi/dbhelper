080fdb355e3554920b4e1734fda7e66c1536e05e
lombars <slombardi@gmail.com>
2016-03-28 00:34:32 -0400

    removed putCallback

--- index.js
+++ index.js
@@ -71,14 +71,6 @@ function DynamoDBHelper(){
     };
 }
 
-function putCallback(err, data) {
-    if (err) {
-        //console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        //console.log("Added item:", JSON.stringify(data, null, 2));
-    }
-};
-
 function cleanJSON(object) {
     for (var i in object) {
         if (object[i] === null || object[i] == '') {
